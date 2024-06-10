42604d0f48a246d1c887665f8fe1471f4f558d56
rgmendezr <raul301095@gmail.com>
2024-06-09 18:25:43 -0600

    v0..1

--- v0.1/save/cnx.php
+++ v0.1/save/cnx.php
@@ -4,7 +4,7 @@ $host = "localhost";
 $usuario = "root";
 $contrasena = "";
 $nombreBaseDatos = "sami";
-$edit_base_url = "http://localhost/GoofyStores/Sami/Sami/v1.0/admin/?key_code=";
+$edit_base_url = "http://localhost/GoofyStores/Sami/Sami/v0.1/admin/?key_code=";
 
 $conexion = new mysqli($host, $usuario, $contrasena, $nombreBaseDatos);
 if ($conexion->connect_error) {
